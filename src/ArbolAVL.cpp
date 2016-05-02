3a20331bb586e1f0b69645d14c84c1548c342022
leandevesa <leandevesa@gmail.com>
2016-05-02 16:37:13 -0700

    Refactor general

--- src/ArbolAVL.cpp
+++ src/ArbolAVL.cpp
@@ -8,156 +8,128 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-// An AVL tree node
-struct node
+struct nodo
 {
-	int key;
-	struct node *left;
-	struct node *right;
+	int clave;
+	nodo *izquierdo;
+	nodo *derecho;
 	int height;
 };
 
-// A utility function to get maximum of two integers
-int max(int a, int b);
-
-// A utility function to get height of the tree
-int height(struct node *N)
+int height(nodo *N)
 {
 	if (N == NULL)
 		return 0;
+
 	return N->height;
 }
 
-// A utility function to get maximum of two integers
 int max(int a, int b)
 {
 	return (a > b)? a : b;
 }
 
-/* Helper function that allocates a new node with the given key and
-	NULL left and right pointers. */
-struct node* newNode(int key)
+nodo* nuevoNodo(int clave)
 {
 
-	node* tmpNode = new node();
+	nodo* unNodo = new nodo();
 
-	tmpNode->key = key;
-	tmpNode->left = NULL;
-	tmpNode->right = NULL;
-	tmpNode->height = 1;
-	return(tmpNode);
+	unNodo->clave = clave;
+	unNodo->izquierdo = NULL;
+	unNodo->derecho = NULL;
+	unNodo->height = 1;
+
+	return(unNodo);
 }
 
-// A utility function to right rotate subtree rooted with y
-// See the diagram given above.
-node *rightRotate(struct node *y)
+nodo *rotacionDerecha(nodo *y)
 {
-	node* x = y->left;
-	node* T2 = x->right;
+	nodo* x = y->izquierdo;
+	nodo* T2 = x->derecho;
 
-	// Perform rotation
-	x->right = y;
-	y->left = T2;
+	x->derecho = y;
+	y->izquierdo = T2;
 
-	// Update heights
-	y->height = max(height(y->left), height(y->right))+1;
-	x->height = max(height(x->left), height(x->right))+1;
+	y->height = max(height(y->izquierdo), height(y->derecho))+1;
+	x->height = max(height(x->izquierdo), height(x->derecho))+1;
 
-	// Return new root
 	return x;
 }
 
-// A utility function to left rotate subtree rooted with x
-// See the diagram given above.
-node *leftRotate(struct node *x)
+nodo *rotacionIzquierda(nodo *x)
 {
-	node *y = x->right;
-	node *T2 = y->left;
+	nodo *y = x->derecho;
+	nodo *T2 = y->izquierdo;
 
-	// Perform rotation
-	y->left = x;
-	x->right = T2;
+	y->izquierdo = x;
+	x->derecho = T2;
 
-	// Update heights
-	x->height = max(height(x->left), height(x->right))+1;
-	y->height = max(height(y->left), height(y->right))+1;
+	x->height = max(height(x->izquierdo), height(x->derecho))+1;
+	y->height = max(height(y->izquierdo), height(y->derecho))+1;
 
-	// Return new root
 	return y;
 }
 
-// Get Balance factor of node N
-int getBalance(struct node *N)
+int getBalance(nodo *N)
 {
 	if (N == NULL)
 		return 0;
-	return height(N->left) - height(N->right);
+	return height(N->izquierdo) - height(N->derecho);
 }
 
-node* insert(struct node* node, int key)
+nodo* insert(nodo* nodo, int clave)
 {
-	/* 1. Perform the normal BST rotation */
-	if (node == NULL)
-		return(newNode(key));
+	if (nodo == NULL)
+		return(nuevoNodo(clave));
 
-	if (key < node->key)
-		node->left = insert(node->left, key);
+	if (clave < nodo->clave)
+		nodo->izquierdo = insert(nodo->izquierdo, clave);
 	else
-		node->right = insert(node->right, key);
+		nodo->derecho = insert(nodo->derecho, clave);
 
-	/* 2. Update height of this ancestor node */
-	node->height = max(height(node->left), height(node->right)) + 1;
+	nodo->height = max(height(nodo->izquierdo), height(nodo->derecho)) + 1;
 
-	/* 3. Get the balance factor of this ancestor node to check whether
-	this node became unbalanced */
-	int balance = getBalance(node);
+	int balance = getBalance(nodo);
 
-	// If this node becomes unbalanced, then there are 4 cases
+	// If this nodo becomes unbalanced, then there are 4 cases
 
-	// Left Left Case
-	if (balance > 1 && key < node->left->key)
-		return rightRotate(node);
+	if (balance > 1 && clave < nodo->izquierdo->clave)
+		return rotacionDerecha(nodo);
 
-	// Right Right Case
-	if (balance < -1 && key > node->right->key)
-		return leftRotate(node);
+	if (balance < -1 && clave > nodo->derecho->clave)
+		return rotacionIzquierda(nodo);
 
-	// Left Right Case
-	if (balance > 1 && key > node->left->key)
+	if (balance > 1 && clave > nodo->izquierdo->clave)
 	{
-		node->left = leftRotate(node->left);
-		return rightRotate(node);
+		nodo->izquierdo = rotacionIzquierda(nodo->izquierdo);
+		return rotacionDerecha(nodo);
 	}
 
-	// Right Left Case
-	if (balance < -1 && key < node->right->key)
+	if (balance < -1 && clave < nodo->derecho->clave)
 	{
-		node->right = rightRotate(node->right);
-		return leftRotate(node);
+		nodo->derecho = rotacionDerecha(nodo->derecho);
+		return rotacionIzquierda(nodo);
 	}
 
-	/* return the (unchanged) node pointer */
-	return node;
+	return nodo;
 }
 
 // A utility function to print preorder traversal of the tree.
-// The function also prints height of every node
-void preOrder(struct node *root)
+// The function also prints height of every nodo
+void preOrder(nodo *root)
 {
 	if(root != NULL)
 	{
-		printf("%d ", root->key);
-		preOrder(root->left);
-		preOrder(root->right);
+		printf("%d ", root->clave);
+		preOrder(root->izquierdo);
+		preOrder(root->derecho);
 	}
 }
 
-/* Drier program to test above function*/
 int main()
 {
-node *root = NULL;
+nodo *root = NULL;
 
-/* Constructing tree given in the above figure */
 root = insert(root, 10);
 root = insert(root, 20);
 root = insert(root, 30);
