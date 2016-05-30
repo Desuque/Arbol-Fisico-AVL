56bca11bfef31124aec5c48d584203bdb1392305
leandevesa <leandevesa@gmail.com>
2016-05-30 13:37:10 -0700

    Avanaces en persistencia

--- src/ArbolesFisicos.cpp
+++ src/ArbolesFisicos.cpp
@@ -6,12 +6,13 @@
 
 using namespace std;
 
-void creg(int id, char codigo [3], ArbolAVL* arbol) {
+void creg(int id, char codigo [3], char descr [1000], ArbolAVL* arbol) {
 	Registro* unRegistro;
 
 	unRegistro = new Registro();
 	unRegistro->id = id;
 	strcpy(unRegistro->codigo, codigo);
+	strcpy(unRegistro->descripcion, descr);
 	arbol->insertar(unRegistro);
 }
 
@@ -21,16 +22,16 @@ int main() //int argc, char *argv[] -> como parametro para la interfaz
 
 	//Interfaz* interfaz = new Interfaz(argc, argv);
 
-	creg(1, "asd", arbol);
-	creg(2, "zzz", arbol);
-	creg(3, "xxx", arbol);
+	creg(9, "AAX", "Descripcion larga 1", arbol);
+	creg(10, "APL", "Descripcion larga 2", arbol);
+	creg(11, "ZUC", "Descripcion larga 3", arbol);
 
 	Persistencia pers;
-	//pers.grabar(arbol);
+	pers.grabar(arbol);
 
 	arbol->print();
 
-	//pers.cargar();
+	pers.cargar();
 	return 0;
 }
 
--- src/Persistencia.cpp
+++ src/Persistencia.cpp
@@ -16,12 +16,12 @@ void Persistencia::grabar(ArbolAVL* unArbol) {
 
 	  Nodo* unNodo = unArbol->getRaiz();
 	  Registro memreg;
-	  Registro* unRegistro = new Registro();
+	  Registro* unRegistro;
 
 		list<Registro*>* registros = unNodo->getRegistros();
 		for(list<Registro*>::iterator list_iter = registros->begin(); list_iter != registros->end(); list_iter++) {
 			unRegistro = *list_iter;
-			myFile.write((char*)&memreg, sizeof(Registro));
+			myFile.write((char*)unRegistro, sizeof(Registro));
 		}
 
 	  myFile.flush();
@@ -30,10 +30,14 @@ void Persistencia::grabar(ArbolAVL* unArbol) {
 
 void Persistencia::cargar() {
 	  fstream myFile ("myfile.bin", ios::in | ios::binary);
-	  Registro* unRegistro;
+	  Registro unRegistro;
 
-	    myFile.seekg (0);
-	    myFile.read ((char*)unRegistro, sizeof (Registro) * 10);
+	  myFile.seekg (ios::beg);
 
-	  std::cout<<unRegistro->id<<endl;
+	  myFile.read ((char*)&unRegistro, sizeof(Registro));
+	  std::cout<<unRegistro.codigo<<endl;
+
+	  if (strcmp(unRegistro.codigo, "AAX") == 0) {
+		  std::cout<<"Piola"<<endl;
+	  }
 }
